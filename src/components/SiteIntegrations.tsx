7726bacc5e8b1f317310f213d9a2b025194a5dc9
Sunand Garg <sunandgarg@Sunands-MacBook-Air.local>
2026-08-28 15:26:59 +0530

    Keep Cloudflare frontend on static AWS API mode

--- src/components/SiteIntegrations.tsx
+++ src/components/SiteIntegrations.tsx
@@ -123,8 +123,9 @@ export function SiteIntegrations() {
       if (map.linkedin_partner_id) {
         inline("li-init", `_linkedin_partner_id="${map.linkedin_partner_id}";window._linkedin_data_partner_ids=window._linkedin_data_partner_ids||[];window._linkedin_data_partner_ids.push(_linkedin_partner_id);(function(l){if(!l){window.lintrk=function(a,b){window.lintrk.q.push([a,b])};window.lintrk.q=[]}var s=document.getElementsByTagName("script")[0];var b=document.createElement("script");b.type="text/javascript";b.async=true;b.src="https://snap.licdn.com/li.lms-analytics/insight.min.js";s.parentNode.insertBefore(b,s);})(window.lintrk);`);
       }
-      // Meta Pixel
-      if (map.facebook_pixel_id) {
+      // Meta Pixel is managed by GTM when a container is enabled. Initializing
+      // it here as well records duplicate PageView events.
+      if (map.facebook_pixel_id && !hasGtm) {
         inline("fbq-init", `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${map.facebook_pixel_id}');fbq('track','PageView');`);
       }
       // Hotjar
